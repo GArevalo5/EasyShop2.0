c67757ebc9292a2ae4882826ad21365c9ae6d266
Fahd <fahds2005@icloud.com>
2024-07-09 15:44:28 -0700

    fixed bugs

--- capstone-starter/src/main/java/org/yearup/controllers/ProfileController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ProfileController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -29,6 +30,7 @@ public class ProfileController
         this.userDao = userDao;
     }
 
+    @GetMapping("/profile")
     public Profile getByid(Principal principal)
     {
         try
